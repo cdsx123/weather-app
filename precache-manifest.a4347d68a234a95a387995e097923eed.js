ebc6013a47b3a77dd937d5527711dd38ebadc351
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2020-08-25 21:59:32 -0700

    Updates

---
+++ precache-manifest.a4347d68a234a95a387995e097923eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26a02e429705084dd46d3919cc09672",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e584860084b5866fb984",
+    "url": "/weather-app/static/css/main.d9265e2d.chunk.css"
+  },
+  {
+    "revision": "562705fa24b50ca4d4e7",
+    "url": "/weather-app/static/js/2.a0530719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.a0530719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e584860084b5866fb984",
+    "url": "/weather-app/static/js/main.80a486d8.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
